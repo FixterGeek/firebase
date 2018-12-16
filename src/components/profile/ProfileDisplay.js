448706eea56758472af46a4ab58decbe068bf29a
DeividZavala <crusedmoss@gmail.com>
2018-12-16 16:54:46 -0600

    solving merge conflicts

--- src/components/profile/ProfileDisplay.js
+++ src/components/profile/ProfileDisplay.js
@@ -5,53 +5,33 @@ import {Link} from 'react-router-dom';
 import NavProfile from '../nav/NavProfile';
 import {CardProfileDisplay} from './CardProfileDisplay';
 import Card from '../card/Card';
-import {Button} from 'antd'
-import { Link } from 'react-router-dom';
 
-<<<<<<< HEAD
 export const ProfileDisplay = ({user}) => {
-   const { enrolled=[]} = user
-=======
-export const ProfileDisplay = ({user={}}) => {
-   const {photoURL, displayName, email, uid} = user
-   let enrolled = []
-    if(user.enrolled) enrolled = Object.keys(user.enrolled)
-   //se tienen que traer los cursos de la bd
-   console.log(enrolled)
->>>>>>> d42ff3b037ca657e16dab7395e8bfaed12808716
-return(
+	const { enrolled=[]} = user
+	return(
 
-    <div className="perfil">
-        <NavProfile user={user} />
-        <div className="fl mg">
-            <CardProfileDisplay user={user} />
-            <div className="profile_cursos">
-<<<<<<< HEAD
-=======
-                {/* {enrolled.map((c,index)=>{
-                    return (
-                        <Card key={index} {...user.enrolled[c]} />
-                    )
-                })} */}
-                {enrolled.length < 1 && 
-                <h2 style={{color:"white"}}>
-                <Link to="/courses" >
-                <Button style={{background:'black', color:"white"}} >
-                     Consigue tu primer curso
-                </Button>
-                </Link>
-                
-                
-                </h2>}
->>>>>>> d42ff3b037ca657e16dab7395e8bfaed12808716
+		<div className="perfil">
+			<NavProfile user={user} />
+			<div className="fl mg">
+				<CardProfileDisplay user={user} />
+				<div className="profile_cursos">
 
-                <div className="cursos_box">
+					<div className="cursos_box">
+						{enrolled.map((c,index)=>{
+							return (
+								<div>
+									<h2>Mis cursos</h2>
+									<br/>
+									<Card key={index} {...c} /></div>
+							)
+						})}
+						{enrolled.length < 1 && <div><h2 style={{color:"#ccc",  fontFamily: 'Muli'}}>Consigue tu primer curso <Link to="/courses" className="st">¡Ahora!</Link></h2></div> }
 
 
-            </div>
-            </div>
-        </div>
+					</div>
+				</div>
+			</div>
 
-    </div>
-)
+		</div>
+	)
 }
