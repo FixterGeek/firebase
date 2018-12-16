7613b99324164405cd4911e3a80fcde808ca3a7a
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2018-12-16 17:03:20 -0600

    badge image

--- src/components/profile/ProfileDisplay.js
+++ src/components/profile/ProfileDisplay.js
@@ -1,57 +1,42 @@
-import React from 'react';
-import './Profile.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import "./Profile.css";
+import { Link } from "react-router-dom";
 //import logo from '../../assets/firemx.png';
-import NavProfile from '../nav/NavProfile';
-import {CardProfileDisplay} from './CardProfileDisplay';
-import Card from '../card/Card';
-import {Button} from 'antd'
-import { Link } from 'react-router-dom';
+import NavProfile from "../nav/NavProfile";
+import { CardProfileDisplay } from "./CardProfileDisplay";
+import Card from "../card/Card";
+import { Button } from "antd";
 
-<<<<<<< HEAD
-export const ProfileDisplay = ({user}) => {
-   const { enrolled=[]} = user
-=======
-export const ProfileDisplay = ({user={}}) => {
-   const {photoURL, displayName, email, uid} = user
-   let enrolled = []
-    if(user.enrolled) enrolled = Object.keys(user.enrolled)
-   //se tienen que traer los cursos de la bd
-   console.log(enrolled)
->>>>>>> d42ff3b037ca657e16dab7395e8bfaed12808716
-return(
-
+export const ProfileDisplay = ({ user = {} }) => {
+  const { photoURL, displayName, email, uid } = user;
+  let enrolled = [];
+  if (user.enrolled) enrolled = Object.keys(user.enrolled);
+  //se tienen que traer los cursos de la bd
+  console.log(enrolled);
+  return (
     <div className="perfil">
-        <NavProfile user={user} />
-        <div className="fl mg">
-            <CardProfileDisplay user={user} />
-            <div className="profile_cursos">
-<<<<<<< HEAD
-=======
-                {/* {enrolled.map((c,index)=>{
+      <NavProfile user={user} />
+      <div className="fl mg">
+        <CardProfileDisplay user={user} />
+        <div className="profile_cursos">
+          {/* {enrolled.map((c,index)=>{
                     return (
                         <Card key={index} {...user.enrolled[c]} />
                     )
                 })} */}
-                {enrolled.length < 1 && 
-                <h2 style={{color:"white"}}>
-                <Link to="/courses" >
-                <Button style={{background:'black', color:"white"}} >
-                     Consigue tu primer curso
+          {enrolled.length < 1 && (
+            <h2 style={{ color: "white" }}>
+              <Link to="/courses">
+                <Button style={{ background: "black", color: "white" }}>
+                  Consigue tu primer curso
                 </Button>
-                </Link>
-                
-                
-                </h2>}
->>>>>>> d42ff3b037ca657e16dab7395e8bfaed12808716
+              </Link>
+            </h2>
+          )}
 
-                <div className="cursos_box">
-
-
-            </div>
-            </div>
+          <div className="cursos_box" />
         </div>
-
+      </div>
     </div>
-)
-}
+  );
+};
